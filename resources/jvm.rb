92c54f6daf997ea5ba4baa9c892b36e462186563
Lonnie VanZandt <lonniev@gmail.com>
2015-06-21 14:35:17 -0600

    No Boolean, use [TrueClass, FalseClass]

--- resources/jvm.rb
+++ resources/jvm.rb
@@ -4,5 +4,5 @@ default_action :install
 attribute :name, kind_of: String, name_attribute: true
 attribute :jvm, kind_of: String, :required => true
 attribute :version, kind_of: String
-attribute :update_path, kind_of: Boolean, :default => true
-attribute :set_home, kind_of: Boolean, :default => true
+attribute :update_path, kind_of: [TrueClass, FalseClass], :default => true
+attribute :set_home, kind_of: [TrueClass, FalseClass], :default => true
